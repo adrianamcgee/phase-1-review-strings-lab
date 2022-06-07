28e00c0387c1152b101745de432f5dfa9fa85b54
Adriana McGee <adriana@Adrianas-MBP.lan>
2022-06-07 09:28:27 -0400

    commit tree 9fc5850157fe87f4e9b0cb451cda4abeeaacb385

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+let currentUser = 'Adriana!';
+let welcomeMessage = 'Welcome to Flatbook, Adriana!';
+currentUser = welcomeMessage;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, `;
+excitedWelcomeMessage = currentUser.toUpperCase();
+let shortGreeting = 'Welcome, !';
